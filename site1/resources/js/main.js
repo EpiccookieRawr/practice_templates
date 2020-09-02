e3febb8690ef8aa3978edfcafccf8c17792e3abd
Chien Wei Chou <bell10142006@hotmail.com>
2020-09-01 21:35:38 -0700

    Finish mobile nav

--- site1/resources/js/main.js
+++ site1/resources/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     /* For the sticky navigation */
     $('.js--explain-section').waypoint(function(direction) {
-        console.log(direction);
         if (direction == "down") {
             $('nav').addClass('sticky');
         } else {
@@ -9,5 +8,17 @@ $(document).ready(function() {
         }
     }, {
       offset: '60px;'
+    });
+
+    //ion-close-round
+    $('.js--nav-icon').click(function() {
+        if($('.js--nav-icon i').hasClass('ion-navicon-round')) {
+            $('.js--nav-icon i').addClass('ion-close-round');
+            $('.js--nav-icon i').removeClass('ion-navicon-round');
+        } else {
+            $('.js--nav-icon i').removeClass('ion-close-round');
+            $('.js--nav-icon i').addClass('ion-navicon-round');
+        }
+        $('.nav-links').toggle( "slow", function() {});
     });
 });
